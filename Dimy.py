38312eda9c889ed685cb789141a1bc8a1f809140
Gordon003 <gordonchen0@gmail.com>
2021-03-23 20:24:29 -0700

    Update Dimy.py

--- Dimy.py
+++ Dimy.py
@@ -5,33 +5,25 @@ import sys
 import threading
 import time
 
-broadcast_id_str = ''
-old_broadcast_id = ''
-
-# create sending and receiving UDP sockets for peer-to-peer
-udp_broadcast_socket = socket(AF_INET, SOCK_DGRAM)
-udp_broadcast_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-
-server_host = ''
-udp_server = socket(AF_INET, SOCK_DGRAM)
-udp_server.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-udp_server.bind((server_host, 55558))
+port = 37020
+broadcast_id_str = ""
 
 print("[STARTING] UDP Broadcaster is starting...")
 
 # Task 1 - 3
 def udp_broadcaster():
-    
-    global broadcast_id_str
-    global old_broadcast_id
-    global udp_broadcast_socket
 
-    host = '255.255.255.255'
-    port = 55558
+    # Create socket
+    udp_broadcast_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
+    udp_broadcast_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+    udp_broadcast_socket.settimeout(0.2)
+
+    global port
+    global broadcast_id_str
 
     # ephID
-    broadcast_id = os.urandom(16)
-    broadcast_id_str = str(binascii.hexlify(broadcast_id), "utf-8")
+    id = os.urandom(16)
+    broadcast_id_str = str(binascii.hexlify(id), "utf-8")
     print(f"Make new ID: {broadcast_id_str}")
 
     # timer
@@ -45,31 +37,30 @@ def udp_broadcaster():
         # broadcast id every 10 seconds
         if curr_timer > broadcast_timer:
             print(f"Broadcast ID: {broadcast_id_str}")
-            udp_broadcast_socket.sendto(broadcast_id, (host, port))
+            udp_broadcast_socket.sendto(id, ('<broadcast>', port))
             broadcast_timer += 10
         # create new id every minute
         elif curr_timer > id_timer:
-            old_broadcast_id = broadcast_id_str
-            broadcast_id = os.urandom(16)
-            broadcast_id_str = str(binascii.hexlify(broadcast_id), "utf-8")
+            id = os.urandom(16)
+            broadcast_id_str = str(binascii.hexlify(id), "utf-8")
             print(f"Make new ID: {broadcast_id_str}")
             id_timer += 60
 
         curr_timer = time.time() - start_time
 
 def udp_receiver():
+    udp_server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) # UDP
+    udp_server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
-    global udp_server
-    global old_broadcast_id
+    global port
     global broadcast_id_str
+    udp_server.bind(("", port))
 
-    # print("Listening")
     while True:
         recv_id, recv_addr = udp_server.recvfrom(2048)
         recv_id = str(binascii.hexlify(recv_id), "utf-8")
-        # print(old_broadcast_id)
-        if broadcast_id_str != recv_id and broadcast_id_str != old_broadcast_id:
-            print("Received ID: ", recv_id, recv_addr)
+        if broadcast_id_str != recv_id:
+            print("Received ID: ", recv_id)
 
 # thread for listening for beacons
 udp_broad_thread = threading.Thread(name = "ClientBroadcaster", target = udp_broadcaster, daemon = True)
