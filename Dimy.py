d4b89f1456cdea039481a26d970037393c3948b1
Gordon003 <gordonchen0@gmail.com>
2021-03-27 23:51:56 +1100

    Update Dimy.py

--- Dimy.py
+++ Dimy.py
@@ -1,19 +1,16 @@
-from binascii import unhexlify
+from ephid import *
 from Crypto.Cipher import DES
-from decimal import Decimal
-from math import ceil
-import os
-import random
+from shamir import *
 from socket import *
-import sys
 import threading
 import time
-from uuid import uuid4
 
 # gLobal Variable
 port = 37020
 broadcast_id_shares = []
-FIELD_SIZE = 100000
+
+g = 2583682
+x = 0
 
 broadcast_key = 'fecdba98'
 broadcast_iv = '01234567'
@@ -22,62 +19,19 @@ broadcast_des2 = DES.new(broadcast_key, DES.MODE_CBC, broadcast_iv)
 
 print("[STARTING] UDP Broadcaster is starting...")
 
-def reconstruct_secret(shares):
-    sums = 0
-    prod_arr = []
- 
-    for j, share_j in enumerate(shares):
-        xj, yj = share_j
-        prod = Decimal(1)
-
-        prod *= yj
-        
-        for i, share_i in enumerate(shares):
-            xi, _ = share_i
-            if i != j:
-                prod *= Decimal(Decimal(xi)/(xi-xj))
- 
-        sums += Decimal(prod)
- 
-    return int(round(Decimal(sums), 0))
- 
-# calculate value of poly at x
-def calculate_poly(x, coefficients):
-    y = 0
-    for coefficient_index, coefficient_value in enumerate(coefficients[::-1]):
-        y += x ** coefficient_index * coefficient_value
-    return y
- 
- # generate 6 shares
-def generate_shares(secret):
-	m = 3
-	n = 6
-
-	# generate polynomials
-	coefficients = [random.randrange(0, FIELD_SIZE) for _ in range(m - 1)]
-	coefficients.append(secret)
-
-	# get 3 random points of polynomials
-	shares = []
-	for i in range(1, n+1):
-		x = random.randrange(1, FIELD_SIZE)
-		shares.append((x, calculate_poly(x, coefficients)))
-
-	return shares
-
 ######################
 
 # broadcast Shares
 def udp_broadcaster():
 
-	global port, broadcast_id_shares, broadcast_des1, broadcast_des2
+	global port, broadcast_id_shares, broadcast_des1, broadcast_des2, x, g
 
 	# create socket
 	broadcast_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 	broadcast_socket.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
 	# create new ephID
-	broadcast_id = str(uuid4().int)[0:16]
+	x, broadcast_id = generate_ephid(g)
 	broadcast_id_shares = generate_shares(int(broadcast_id))
 	broadcast_hash = broadcast_des1.encrypt(broadcast_id)
 	print(f"Make new ID: {broadcast_id}")
@@ -101,7 +55,7 @@ def udp_broadcaster():
 
 		# create new id every minute
 		elif curr_timer > id_timer:
-			broadcast_id = str(uuid4().int)[0:16]
+			x, broadcast_id = generate_ephid(g)
 			broadcast_id_shares = generate_shares(int(broadcast_id))
 			broadcast_hash = broadcast_des1.encrypt(broadcast_id)
 			print(f"Make new ID: {broadcast_id}")
