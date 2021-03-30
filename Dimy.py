190daaf5500515c13e3132718986582448a5eb1e
eushaun <limeushaun99@gmail.com>
2021-03-30 12:52:59 -0400

    task 2-5

--- Dimy.py
+++ Dimy.py
@@ -4,18 +4,20 @@ from shamir import *
 from socket import *
 import threading
 import time
+from hashlib import sha1
 
-# gLobal Variable
-port = 37020
+# global variable
+port = 37025
 broadcast_id_shares = []
 
 g = 2583682
 x = 0
 
-broadcast_key = 'fecdba98'
-broadcast_iv = '01234567'
-broadcast_des1 = DES.new(broadcast_key, DES.MODE_CBC, broadcast_iv)
-broadcast_des2 = DES.new(broadcast_key, DES.MODE_CBC, broadcast_iv)
+# broadcast_key = 'fecdba98'
+# broadcast_iv = '01234567'
+# broadcast_des1 = DES.new(broadcast_key, DES.MODE_CBC, broadcast_iv)
+# broadcast_des2 = DES.new(broadcast_key, DES.MODE_CBC, broadcast_iv)
+broadcast_hash = ""
 
 print("[STARTING] UDP Broadcaster is starting...")
 
@@ -24,7 +26,8 @@ print("[STARTING] UDP Broadcaster is starting...")
 # broadcast Shares
 def udp_broadcaster():
 
-	global port, broadcast_id_shares, broadcast_des1, broadcast_des2, x, g
+	# global port, broadcast_id_shares, broadcast_des1, broadcast_des2, x, g
+	global port, broadcast_id_shares, broadcast_hash, x, g
 
 	# create socket
 	broadcast_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
@@ -33,9 +36,15 @@ def udp_broadcaster():
 	# create new ephID
 	x, broadcast_id = generate_ephid(g)
 	broadcast_id_shares = generate_shares(int(broadcast_id))
-	broadcast_hash = broadcast_des1.encrypt(broadcast_id)
+
+	broadcast_hash = sha1(str(broadcast_id).encode()).hexdigest()
+	# broadcast_hash = broadcast_des1.encrypt(broadcast_id)
+
+	print()
 	print(f"Make new ID: {broadcast_id}")
-	print(broadcast_id_shares)
+	for i, shares in enumerate(broadcast_id_shares):
+		print(f"Share {i+1}: {shares}")
+	print()
 
 	# timer
 	start_time = time.time()
@@ -47,8 +56,8 @@ def udp_broadcaster():
 
 		# broadcast id every 10 seconds
 		if curr_timer > broadcast_timer and len(broadcast_id_shares) != 0:
-			print(f"Broadcast ID: {broadcast_id_shares[0]}")
-			send_str = str(broadcast_id_shares[0][0]) + "|" + str(broadcast_id_shares[0][1]) + "|" + str(broadcast_hash)
+			print(f"Broadcast share: {broadcast_id_shares[0]}")
+			send_str = str(broadcast_id_shares[0][0]) + "|" + str(broadcast_id_shares[0][1]) + "|" + broadcast_hash
 			broadcast_socket.sendto(send_str.encode('utf-8'), ('192.168.4.255', port))
 			broadcast_id_shares.pop(0)
 			broadcast_timer += 3
@@ -57,16 +66,23 @@ def udp_broadcaster():
 		elif curr_timer > id_timer:
 			x, broadcast_id = generate_ephid(g)
 			broadcast_id_shares = generate_shares(int(broadcast_id))
-			broadcast_hash = broadcast_des1.encrypt(broadcast_id)
+
+			broadcast_hash = sha1(str(broadcast_id).encode()).hexdigest()
+			# broadcast_hash = broadcast_des1.encrypt(broadcast_id)
+
+			print()
 			print(f"Make new ID: {broadcast_id}")
-			print(broadcast_id_shares)
+			for i, shares in enumerate(broadcast_id_shares):
+				print(f"Share {i+1}: {shares}")
+			print()
 			id_timer += 18
 
 		curr_timer = time.time() - start_time
 
 def udp_receiver():
 
-	global port, broadcast_id, broadcast_des2
+	# global port, broadcast_id, broadcast_des2
+	global port, broadcast_hash, x
 	
 	new_contact_list = {}
 
@@ -80,26 +96,59 @@ def udp_receiver():
 		# receive message
 		recv_msg, recv_addr = server_socket.recvfrom(2048)
 		recv_x, recv_y, recv_hash = recv_msg.decode("utf-8").split("|")
-		print(f"Received ({recv_x}, {recv_y}) - Hash = {recv_hash}")
-		
-		recv_x = int(recv_x)
-		recv_y = int(recv_y)
-		
-		if recv_hash not in new_contact_list.keys():
-			new_contact_list[recv_hash] = [(recv_x, recv_y)]
+
+		# skip if receive own message
+		if recv_hash == broadcast_hash:
+			continue
 		else:
-			new_contact_list[recv_hash].append((recv_x, recv_y))
-		
-		# Check if the hash contains 3 entries 
-		new_dict = {}
-		for curr_hash in new_contact_list.keys():
-			lst = new_contact_list[curr_hash]
-			if len(lst) == 3:
-				sec = reconstruct_secret(lst)
-				print(sec)
+			# print(f"Received ({recv_x}, {recv_y}) - Hash = {recv_hash}")
+			recv_x = int(recv_x)
+			recv_y = int(recv_y)
+			
+			if recv_hash not in new_contact_list.keys():
+				new_contact_list[recv_hash] = [(recv_x, recv_y)]
 			else:
-				new_dict[curr_hash] = lst
-		new_contact_list = new_dict
+				new_contact_list[recv_hash].append((recv_x, recv_y))
+			
+			# keep track of number of shares received
+			num_shares = len(new_contact_list[recv_hash])
+			print()
+			print(f"Received {num_shares} shares for {recv_hash}.")
+			print()
+			
+			# Check if the hash contains 3 entries
+			if num_shares == 3:
+				sec = reconstruct_secret(new_contact_list[recv_hash])
+				print()
+				print(f"Reconstructing EphID: {sec}")
+				print("Verifying integrity of EphID...")
+				new_hash = sha1(str(sec).encode()).hexdigest()
+				print(f"Received hash: {recv_hash}")
+				print(f"Recontructed hash: {new_hash}")
+				if recv_hash == new_hash:
+					print("Verified hash. Computing EncID...")
+					enc_id = sec * x
+					print(f"EncID is: {enc_id}")
+				print()
+
+			# # Check if the hash contains 3 entries 
+			# new_dict = {}
+			# for curr_hash in new_contact_list.keys():
+			# 	lst = new_contact_list[curr_hash]
+			# 	if len(lst) == 3:
+			# 		sec = reconstruct_secret(lst)
+			# 		print(f"Reconstructing EphID: {sec}")
+			# 		print("Verifying integrity of EphID...")
+			# 		new_hash = sha1(str(sec).encode()).hexdigest()
+			# 		print(f"Received hash: {recv_hash}")
+			# 		print(f"Recontructed hash: {new_hash}")
+			# 		if recv_hash == new_hash:
+			# 			print("Verified hash. Computing EncID...")
+			# 			enc_id = sec * x
+			# 			print(f"EncID is: {enc_id}")
+			# 	else:
+			# 		new_dict[curr_hash] = lst
+			# new_contact_list = new_dict
 			
 
 # thread for listening for beacons
