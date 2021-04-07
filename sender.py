975f6887912b647d5c825299263d966c82839949
Gordon003 <gordonchen0@gmail.com>
2021-04-07 13:18:44 +1000

    Update sender.py

--- sender.py
+++ sender.py
@@ -1,14 +1,32 @@
 import requests
 from requests.structures import CaseInsensitiveDict
+import base64
 
-url = "http://ec2-3-25-246-159.ap-southeast-2.compute.amazonaws.com:9000/comp4337/cbf/upload"
+def send_qbf(qbf):
+	url = "http://ec2-3-25-246-159.ap-southeast-2.compute.amazonaws.com:9000/comp4337/cbf/upload"
 
-headers = CaseInsensitiveDict()
-headers["Content-Type"] = "application/json"
+	headers = CaseInsensitiveDict()
+	headers["Content-Type"] = "application/json"
+	data = '{{QBF: {0}}}'.format(base64.b64encode(bytes(qbf, encoding="ascii")))
+	resp = requests.post(url, headers=headers, data='=TExMTExMjM1MjIzcQ==K')
 
-data = '{"qbf": "asdad"}'
+	return resp
 
+def send_cbf():
+	URL = "http://ec2-3-25-246-159.ap-southeast-2.compute.amazonaws.com:9000/comp4337/cbf/upload"
 
-resp = requests.post(url, headers=headers, data=data)
+	headers = CaseInsensitiveDict()
+	headers["Content-Type"] = "application/json"
+	data = '{{CBF: {0}}}'.format(base64.b64encode(bytes(qbf, encoding="ascii")))
+	resp = requests.post(url, headers=headers, data='=TExMTExMjM1MjIzcQ==K')
 
-print(resp.status_code)
+	return resp
+
+# main function
+if __name__ == "__main__":
+
+	asd = "10" * 400000
+	resp = send_qbf(asd)
+	print(len(asd))
+
+	print(resp.text)
