f9cf83637efbff0b2bb3668a4cd5ab750ef1029b
Gordon003 <gordonchen0@gmail.com>
2021-04-21 23:09:22 +1000

    Update shamir.py

--- shamir.py
+++ shamir.py
@@ -1,5 +1,3 @@
-# pip install pycryptodome
-
 from Crypto.Protocol.SecretSharing import Shamir
 from binascii import hexlify, unhexlify
 
@@ -11,49 +9,53 @@ def generate_shares(secret):
 def reconstruct_secret(shares):
     return Shamir.combine(shares)
 
+'''
+OLD METHODS - ONLY WORKS WITH INTEGER
 
-# FIELD_SIZE = 100000
+FIELD_SIZE = 100000
 
-# # calculate value of poly at x
-# def calculate_poly(x, coefficients):
-#     y = 0
-#     for coefficient_index, coefficient_value in enumerate(coefficients[::-1]):
-#         y += x ** coefficient_index * coefficient_value
-#     return y
+# calculate value of poly at x
+def calculate_poly(x, coefficients):
+    y = 0
+    for coefficient_index, coefficient_value in enumerate(coefficients[::-1]):
+        y += x ** coefficient_index * coefficient_value
+    return y
  
-#  # generate 6 shares
-# def generate_shares(secret):
-# 	m = 3
-# 	n = 6
+# generate 6 shares
+def generate_shares(secret):
+    m = 3
+    n = 6
 
-# 	# generate polynomials
-# 	coefficients = [random.randrange(0, FIELD_SIZE) for _ in range(m - 1)]
-# 	coefficients.append(secret)
+    # generate polynomials
+    coefficients = [random.randrange(0, FIELD_SIZE) for _ in range(m - 1)]
+    coefficients.append(secret)
 
-# 	# get 3 random points of polynomials
-# 	shares = []
-# 	for i in range(1, n+1):
-# 		x = random.randrange(1, FIELD_SIZE)
-# 		shares.append((x, calculate_poly(x, coefficients)))
+    # get 3 random points of polynomials
+    shares = []
+    for i in range(1, n+1):
+        x = random.randrange(1, FIELD_SIZE)
+        shares.append((x, calculate_poly(x, coefficients)))
 
-# 	return shares
+    return shares
 
-# # reconstruct id
-# def reconstruct_secret(shares):
-#     sums = 0
-#     prod_arr = []
+# reconstruct id
+def reconstruct_secret(shares):
+    sums = 0
+    prod_arr = []
  
-#     for j, share_j in enumerate(shares):
-#         xj, yj = share_j
-#         prod = Decimal(1)
+    for j, share_j in enumerate(shares):
+        xj, yj = share_j
+        prod = Decimal(1)
 
-#         prod *= yj
+        prod *= yj
         
-#         for i, share_i in enumerate(shares):
-#             xi, _ = share_i
-#             if i != j:
-#                 prod *= Decimal(Decimal(xi)/(xi-xj))
- 
-#         sums += Decimal(prod)
-#         print(sums)
-#     return int(round(Decimal(sums), 0))
+        for i, share_i in enumerate(shares):
+            xi, _ = share_i
+            if i != j:
+                prod *= Decimal(Decimal(xi)/(xi-xj))
+                
+            sums += Decimal(prod)
+            print(sums)
+            
+      return int(round(Decimal(sums), 0))
+'''
